41a9e4f48d9ae460a96612386e83dc1bd289b0cf
svv <vvsh1978@mail.ru>
2022-04-19 00:05:26 +0500

    feat: add show table

--- dom.js
+++ dom.js
@@ -5,9 +5,11 @@ const textInputsOffice = document.querySelectorAll('.office')
 const checkboxInput = document.getElementById('building-property')
 const btnForm = document.getElementById('btn-submit')
 const form = document.querySelector('form')
+const table = document.querySelector('table')
 let inputsEnabled = document.querySelectorAll('input[type=text]:not([disabled])')
+let deleteBtns = table.querySelectorAll('.del-btn')
+let arrData = JSON.parse(localStorage.getItem('dataBuildings')) || []
 
-console.log(form);
 
 selectTypeBuilding.addEventListener('change', () => {
     if (selectTypeBuilding.value === '') {
@@ -42,18 +44,91 @@ selectTypeBuilding.addEventListener('change', () => {
             let empty = [...inputsEnabled].filter(el => {
                 return el.value.trim() === ''
             }).length
-            console.log(empty);
             btnForm.disabled = empty !== 0
         })
     })
 })
 
 form.addEventListener('submit', (e) => {
-    // e.preventDefault()
-    console.log(this);
+    e.preventDefault()
+    let obj
+    if (form[0].value === 'product') {
+        obj = new ProductBuilding(
+            form[1].value,
+            form[2].value,
+            form[3].value,
+            form[4].checked,
+            form[5].value,
+            form[6].value
+        )
+    }
+    if (form[0].value === 'office') {
+        obj = new OfficeBuilding(
+            form[1].value,
+            form[2].value,
+            form[3].value,
+            form[4].checked,
+            form[7].value,
+            form[8].value
+        )
+    }
+    console.log(obj);
+    arrData.push(obj)
+    console.log(arrData);
+    resetForm()
+    addLocalStorage()
+    showTableRow(obj)
 })
 
+const resetForm = () => {
+    form.querySelectorAll('input[type=text]').forEach((item) => {
+        item.value = ''
+        item.disabled = true
+    })
+    form[0].value = ''
+    form[4].checked = false
+    form[4].disabled = true
+    form[9].disabled = true
+}
 
+const addLocalStorage = () => {
+    localStorage.setItem('dataBuildings', JSON.stringify(arrData))
+}
+
+const showTableRow = (obj) => {
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+        <th>${obj._cadastralNum}</th>
+        <th>${obj._name}</th>
+        <th>${obj._area}</th>
+        <th>${obj._property}</th>
+        <th>${obj._nameProduct}</th>
+        <th>${obj._power}</th>
+        <th>${obj._levelNum}</th>
+        <th>${obj._workerNum}</th>
+        <th><button class = "del-btn"></button></th>
+    `
+    table.append(tr)
+    table.querySelectorAll('th').forEach((item) => {
+        if (item.textContent === 'undefined') {
+            item.textContent = '-'
+        }
+    })
+    tr.querySelector('.del-btn').addEventListener('click', (e) => {
+        // Building.deleteObj()
+        console.log(e.target);
+    })
+}
+
+const showTable = (arr) => {
+    arr.forEach((item) => {
+        showTableRow(item)
+    })
+
+
+}
+
+showTable(arrData)
 
 
 
